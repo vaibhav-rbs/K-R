24eb71800e57c760add6345dfc3538ad8f337168
vaibhav-rbs <vaibhav.rbs@gmail.com>
2015-12-25 20:53:36 -0800

    c2f commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+table:
+	rm c2f-table
+	gcc c2f-table.c -o c2f-table
---
+++ c2f-table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int fahr, celcius;
+	int lower, upper, step;
+
+	lower = 0;
+	upper = 300;
+	step = 20;
+
+	fahr = lower;
+	printf("fahrenheight \t celcius");
+
+	while(fahr <= upper)
+	{
+		celcius = 5 * (fahr - 32) / 9;
+		printf("%d\t%d\n",fahr, celcius);
+		fahr = fahr + step;
+	}
+
+}
