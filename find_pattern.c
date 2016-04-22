884f8697cd2f8f22ef088b2c4b49a591cb213d41
vaibhav-rbs <vaibhav@ixsystems.com>
2016-04-21 20:38:25 -0700

    page 69 K&R ebook

---
+++ find_pattern.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAXLINE 1000      /* maximum input line length */
+
+int getline(char line[], int max);
+int strindex(char source[], char searchfor[];
+
+
+char pattern[] = "ould"
+
+main ()
+{
+  char line[MAXLINE];
+  int found = 0;
+
+  while (getline(line, MAXLINE) > 0)
+    if (strindex(line, pattern) >= 0)
+    {
+      printf("%s", line );
+      found ++;
+    }
+    return found;
+}
+
+int getline()
+{
+
+}
+
+int strindex()
+{
+
+}
