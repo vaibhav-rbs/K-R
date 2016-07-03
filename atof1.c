a33b29bb639ff562354b57952fe27d5406bdf901
vaibhav-rbs <vaibhav.rbs@gmail.com>
2016-07-02 17:02:02 -0700

    calculator

--- atof1.c
+++ atof1.c
@@ -1,5 +1,33 @@
 #include <ctype.h>
-#include <printf.h>
+#include <stdio.h>
+
+#define MAXLINE 100
+
+
+int getline1(char s[], int lim)
+{
+	int c,i;
+
+	for(i = 0; i < lim -1 && ((c = getchar()) != EOF && c != '\n'); ++i)
+		/*write all values from console into char array(remember array is modified in place)
+		'i' gets populated at the first step,
+		just in case you are wondering.
+		*/
+		s[i] = c;
+	if (c == '\n')
+		{
+		/* if current char is new line character, 
+		this means line has ended increment i one value
+		*/
+			s[i] = c;
+			++i;
+		}
+	/* adding \0 in the end and modifying the array*/
+	s[i] = '\0';
+	return i;
+	
+	
+}
 
 double atof1(char s[])
 {
@@ -24,6 +52,13 @@ double atof1(char s[])
 
 int main()
 {
-	char input[] = "       -12345.01123141";
-	printf("%f",atof1(input));
+	double sum, atof1(char[]);
+	char line[MAXLINE];
+	int getline1(char line [], int max);
+
+	sum = 0;
+	while (getline1(line, MAXLINE) > 0)
+		printf("\t%f\n", sum += atof1(line));
+	return 0;
+	
 }
